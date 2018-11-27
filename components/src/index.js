faa0fa37e136e1a9e166905929f0c93d7457d2f6
Billi Ramirez <ramirezbilly42@outlook.com>
2018-11-27 09:45:14 -0600

    Showing custom children

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = ()=> {
     return (
       <div className="ui container comments">
-        <CommentDetail/>
-        <CommentDetail/>        
-        <CommentDetail/>        
+        <ApprovalCard>
+          <CommentDetail 
+            author="Sam" 
+            timeAgo="today at 4:50 pm" 
+            content="Nice blog post!" 
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="yesterday at 1:00 pm" 
+          content="Keep trying!" 
+          avatar={faker.image.avatar()}
+        />        
+        <CommentDetail 
+          author="Carlos" 
+          timeAgo="today at 2:33 pm" 
+          content="I love writing" 
+          avatar={faker.image.avatar()}
+        />        
       </div>  
     );
 };
