ba45ce5903ed83ec9fa0f733110acb7c44fa5827
Billi Ramirez <ramirezbilly42@outlook.com>
2018-11-29 13:27:13 -0600

    configuring with mapStateToProps

---
+++ songs/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SongList from './SongList';
+
+import {selectSong} from '../actions';
+
+const App = () => {
+    return (
+        <div>
+            <SongList/>
+        </div>
+    );
+};
+
+export default App;
