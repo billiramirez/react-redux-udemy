d096e21a43b3d69d8e31127706bc0e5ae98f6ca1
Billi Ramirez <ramirezbilly42@outlook.com>
2018-11-30 11:34:38 -0600

    extracting logic to mapStateToProps

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -8,5 +8,10 @@ export const asyncFetchPosts = () => async dispatch => {
 
 
 export const fetchPosts = (response)=> {
-    return {type: 'FETCH_POSTS', payload: response};
+    return {type: 'FETCH_POSTS', payload: response.data};
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+}; 
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { asyncFetchPosts } from '../actions';
 
+import UserHeader from './UserHeader';
+
 
 class PostList extends React.Component{
 
@@ -9,11 +11,28 @@ class PostList extends React.Component{
         this.props.asyncFetchPosts();
     }
 
+    renderList(){
+        return this.props.posts.map( p => {
+            return (
+                <div className="item" key={p.id}>
+                    <i className="larg middle aligned icon user"/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{p.title}</h2>
+                            <p>{p.body}</p>
+                        </div>
+                        <UserHeader userId={p.userId}/>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render(){
         console.log(this.props.posts);
         return (
-            <div>
-                PostList
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div>
         );
     }
