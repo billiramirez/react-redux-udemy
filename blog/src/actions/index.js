de75c1b8937bb5f3aa5c44c7a1eb280d35c3d713
Billi Ramirez <ramirezbilly42@outlook.com>
2018-11-30 11:56:45 -0600

    one time memoization

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonplaceholder from '../apis/jsonplaceholder';
+import _ from 'lodash';
 
 // When we make use use of redux-thunk, we need to make sure of a function return a function
 export const asyncFetchPosts = () => async dispatch => {
@@ -11,7 +12,9 @@ export const fetchPosts = (response)=> {
     return {type: 'FETCH_POSTS', payload: response.data};
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+ 
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonplaceholder.get(`/users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data});
-}; 
+});
