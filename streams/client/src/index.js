651966b897374ba18cb54a7d4b720e31411353f8
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-03 14:49:43 -0600

    recording the userId

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers);
+
+// const composeWithDevTools =  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware())
+    );
 
 ReactDOM.render(
     <Provider store={store}>
