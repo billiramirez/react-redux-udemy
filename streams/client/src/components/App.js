530bc23226ed0532a07c3b6055d3e9e9aa5ff71c
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-03 09:05:26 -0600

    Link inside router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
 
-const PageOne  = ()=>{
-    return (<div>
-        PageOne
-    </div>);
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 
-const PageTwo  = ()=>{
-    return (<div>
-        PageTwo
-    </div>);
-}
 
 const App = () => {
     return (
-        <div> 
+        <div className="ui container"> 
             <BrowserRouter>
             <div>
-                <Route path="/" exact component={PageOne}/>
-                <Route path="/page-two" component={PageTwo}/>
+                <Header/>
+                <Route path="/" exact component={StreamList}/>
+                <Route path="/stream/new" exact component={StreamCreate}/>
+                <Route path="/stream/edit" exact component={StreamEdit}/>
+                <Route path="/stream/delete" exact component={StreamDelete}/>
+                <Route path="/stream/show" exact component={StreamShow}/>
+
             </div>
             </BrowserRouter>    
          </div>
