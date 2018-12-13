369d93b292474145673e726db5008a5e2a54ce42
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-13 15:34:17 -0600

    refactoring

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -12,12 +12,12 @@ class StreamList extends React.Component{
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
-                    <i className="large middle aligned icon camara"/>
+                    <i className="large middle aligned icon camera"/>
                     <div className="content">
                         {stream.title}
-                    </div>
-                    <div className="description">
-                        {stream.description}
+                         <div className="description">
+                            {stream.description}
+                        </div>
                     </div>
                 </div>
             )
