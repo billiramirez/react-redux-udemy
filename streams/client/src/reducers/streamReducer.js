ef2cc244a5da9cf5c5814c71822e1b56bd037536
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-14 12:20:14 -0600

    fetching a stream from edit stream

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -13,22 +13,22 @@ import {
 export default (state = {}, action) => {
     switch(action.type){
 
-        case FETCH_STREAMS:
-            return {...state, ..._.mapKeys(action.payload, 'id')};
+    case FETCH_STREAMS:
+        return { ...state, ..._.mapKeys(action.payload, 'id') };
 
-        case FETCH_STREAM:
-            return {...state, [action.payload.id]: action.payload};
+    case FETCH_STREAM:
+        return { ...state, [action.payload.id]: action.payload };
 
-        case CREATE_STREAM:
-            return {...state, [action.payload.id]: action.payload};
+    case CREATE_STREAM:
+        return { ...state, [action.payload.id]: action.payload };
 
-        case EDIT_STREAM: 
-            return {...state, [action.payload.id]: action.payload};
+    case EDIT_STREAM:
+        return { ...state, [action.payload.id]: action.payload };
 
-        case DELETE_STREAM: 
-            return _.omit(state, action.payload);
-
-        default:
-            return state;
+    case DELETE_STREAM:
+        return _.omit(state, action.payload);
+        
+    default:
+        return state;
     }
 }
