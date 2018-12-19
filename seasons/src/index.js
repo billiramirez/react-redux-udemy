e007b3e2b98fa156e5af5c64a022085b875b2935
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-19 09:22:20 -0600

    extracting reusable logic

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
-
+import useLocation from './useLocation';
 
 
 const  App = ()=> {
 
-    const [lat, setLat] = useState(null);
-    const [errorMessage, setErrorMessage] = useState(null);
-
-    useEffect(()=>{
-        window.navigator.geolocation.getCurrentPosition(
-            position=>  setLat(position.coords.latitude),
-            err =>  setErrorMessage(err.message)
-        );
-    },[])
+    const [lat, errorMessage ] = useLocation();
 
     let content;
     if(errorMessage){
